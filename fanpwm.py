9fac316d02146c84d4b4180e7241bba2fc0eb8c1
Laszlo Molnar <lazlowmiller@gmail.com>
2020-03-11 19:45:04 +0100

    refresh time reduced to 1s

--- fanpwm.py
+++ fanpwm.py
@@ -43,7 +43,7 @@ class PWMPin:
 
 
 class Control:
-    REFRESH_TIME = 3  # seconds
+    REFRESH_TIME = 1  # seconds
 
     def __init__(self, pwm: PWMPin, t_range=(40, 60), pwm_range=(40, 100)):
         self.pwm = pwm
